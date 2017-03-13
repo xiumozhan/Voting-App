92d8a73550d1720cebb9f1a8fdd3b8e12b050ca8
Xiumo Zhan <xiumozhan@gmail.com>
2017-03-12 21:57:09 -0700

    Fix tests for login

--- test/login-test.js
+++ test/login-test.js
@@ -13,78 +13,81 @@ describe('create a user first and test its login validation process', () => {
         password: '123123123123'
     };
 
-    const createUser = User.create(Xiumo);
-
     it('enables a user to login using correct username and password', (done) => {
-        createUser.then(() => {
+        User.create(Xiumo).then(() => {
             request(app)
                 .post('/api/login')
                 .send({
                     usernameOrEmail: Xiumo.name,
                     password: Xiumo.password
                 })
-                .end((res) => {
-                    assert(res.data.valid === true && res.status === 200);
+                .then((res) => {
+                    // console.log(res.body);
+                    assert(res.body.valid === true && res.status === 200);
                     done();
                 });
         });
     });
 
     it('enables a user to login using correct email address and password', (done) => {
-        createUser.then(() => {
+        User.create(Xiumo).then(() => {
             request(app)
                 .post('/api/login')
                 .send({
                     usernameOrEmail: Xiumo.email,
                     password: Xiumo.password
                 })
-                .end((res) => {
-                    assert(res.data.valid === true && res.status === 200);
+                .then((res) => {
+                    // console.log(res.body);
+                    assert(res.body.valid === true && res.status === 200);
                     done();
                 });
         });
     });
 
     it('stops a user from logging in with unregistered username', (done) => {
-        createUser.then(() => {
+        User.create(Xiumo).then(() => {
             request(app)
                 .post('/api/login')
                 .send({
                     usernameOrEmail: 'xiumoz',
                     password: Xiumo.password
                 })
-                .end((res) => {
-                    assert(res.data.valid === false && res.status === 400);
+                .then((res) => {
+                    // console.log(res.body);
+                    assert(res.body.valid === false && res.body.user === undefined && res.status === 400);
                     done();
                 });
         });
     });
 
     it('stops a user from logging in with unregistered email', (done) => {
-        createUser.then(() => {
+        User.create(Xiumo).then(() => {
             request(app)
                 .post('/api/login')
                 .send({
                     usernameOrEmail: 'xiumoz@fcc.com',
                     password: Xiumo.password
                 })
-                .end((res) => {
-                    assert(res.data.valid === false && res.status === 400);
+                .then((res) => {
+                    // console.log(res.body);
+                    assert(res.body.valid === false && res.body.user === undefined && res.status === 400);
                     done();
                 });
         });
     });
 
     it('stops a user from logging in with incorrect password', (done) => {
-        createUser.then(() => {
+        User.create(Xiumo).then(() => {
             request(app)
                 .post('/api/login')
                 .send({
                     usernameOrEmail: Xiumo.name,
                     password: '1231231231'
                 })
-                .end((res) => {
-                    assert(res.data.valid === false && res.status === 400);
+                .then((res) => {
+                    // console.log(res.body);
+                    assert(res.body.valid === false && res.body.user.name === Xiumo.name && res.status === 400);
                     done();
                 });
         });
