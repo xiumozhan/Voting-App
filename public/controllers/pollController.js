ce2ef0c53b086f65b9baf45da3a759ed3526cb50
Xiumo Zhan <xiumozhan@gmail.com>
2017-03-15 23:21:54 -0700

    Write front end controllers for poll page and user profile page

--- public/controllers/pollController.js
+++ public/controllers/pollController.js
@@ -1,3 +1,41 @@
-testApp.controller('pollController', ['$scope', function($scope) {
-    $scope.title = 'poll';
+testApp.controller('pollController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    const pollId = $routeParams.id;
+    const getAllOptions = () => {
+        if(pollId) {
+            $http({
+                method: 'GET',
+                url: `/api/poll/${pollId}`,
+            })
+            .then((poll) => {
+                console.log('the newly created poll:');
+                console.log(poll);
+                const pollObj = poll.data;
+                $scope.title = pollObj.question;
+                $scope.options = pollObj.options;
+            }, (fail) => {
+                console.log(fail);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+    };
+
+    getAllOptions();
+
+    $scope.vote = function(optionId) {
+        $http({
+            method: 'PUT',
+            url: `/api/option/${optionId}`
+        })
+        then((option) => {
+            const optionObj = option.data;
+            console.log(optionObj);
+        }, (fail) => {
+            console.log(fail);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
 }]);
--- public/controllers/profileController.js
+++ public/controllers/profileController.js
@@ -1,3 +1,78 @@
-testApp.controller('profileController', ['$scope', function($scope) {
-    $scope.title = 'profile';
+testApp.controller('profileController', ['$scope', '$window', '$location', 'jwtHelper', '$http', function($scope, $window, $location, jwtHelper, $http) {
+    const tokenPayload = jwtHelper.decodeToken($window.localStorage['token']);
+    console.log(tokenPayload);
+
+    $scope.username = tokenPayload.user.name;
+    // $scope.polls = tokenPayload.user.polls;
+
+    $scope.logOut = function() {
+        delete $window.localStorage['token'];
+        $location.path('/');
+    };
+
+    const getPollsCreatedByUser = () => {
+        if(tokenPayload) {
+            $http({
+                method: 'GET',
+                url: '/api/user/polls',
+                params: {
+                    id: tokenPayload.user.id
+                }
+            })
+            .then((polls) => {
+                $scope.polls = polls.data;
+                console.log($scope.polls);
+            }, (fail) => {
+                console.log(fail);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+    };
+
+    getPollsCreatedByUser();
+
+    $scope.goToPoll = function(id) {
+        $location.path(`/poll/${id}`);
+    };
+
+    $scope.createPoll = function() {
+        $http({
+            method: 'POST',
+            url: '/api/polls',
+            data: {
+                question: $scope.pollQuestion,
+                user: {
+                    name: $scope.username
+                }
+            }
+        })
+        .then((response) => {
+            const polls = response.data.polls;
+            console.log('newly created poll:');
+            console.log(polls);
+            $location.path(`/poll/${polls[polls.length - 1]}`);
+        }, (fail) => {
+            console.log(fail);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+
+    $scope.deletePoll = function(id) {
+        $http({
+            method: 'DELETE',
+            url: `/api/poll/${id}`
+        })
+        .then((response) => {
+            console.log(response);
+        }, (fail) => {
+            console.log(fail);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
 }]);
